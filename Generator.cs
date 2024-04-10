915a10e37cca70825dbf9ffff79bdb589ee4319d
Thomas Donahue <donahue777@yahoo.com>
2024-04-09 20:44:10 -0400

    Add project files.

---
+++ Generator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COD
+{
+    public class Generator
+    {
+        public static IRestResponse CallOfDuty()
+        {
+            var client = new RestSharp.RestClient("");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("");
+            request.AddHeader("");
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
